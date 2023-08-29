8dc034374aad1ddeca48ba384163b50ae38fb5d0
Timo Bemmer <8bemmer@informatik.uni-hamburg.de>
2023-08-29 13:50:33 +0200

    factor vote into its own mod

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::path::PathBuf;
 
 use serde::Deserialize;
-use sqlx::{Executor, PgPool};
+use sqlx::PgPool;
+
+use vote::Vote;
+
+mod vote;
 
 #[derive(Debug, Clone)]
 struct Model {
@@ -39,75 +43,6 @@ async fn tide(
 #[derive(Deserialize)]
 struct Join {
     code: String,
-}
-
-#[derive(Debug, Deserialize)]
-struct Vote {
-    title: String,
-    #[serde(deserialize_with = "deserialize_choices")]
-    choices: Vec<String>,
-    #[serde(rename(deserialize = "max-choices"))]
-    max_choices: usize,
-    #[serde(default)]
-    anonymous: bool,
-    #[serde(deserialize_with = "deserialize_password")]
-    password: Option<String>,
-}
-
-use serde::Deserializer;
-
-fn deserialize_password<'de, D>(deserializer: D) -> Result<Option<String>, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    struct V;
-    impl<'de> serde::de::Visitor<'de> for V {
-        type Value = Option<String>;
-
-        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-            formatter.write_str("a string")
-        }
-
-        fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-        where
-            E: serde::de::Error,
-        {
-            Ok((!v.is_empty()).then(|| v.to_string()))
-        }
-    }
-
-    deserializer.deserialize_str(V)
-}
-
-const SEPARATOR: char = '\x1F';
-
-fn deserialize_choices<'de, D>(deserializer: D) -> Result<Vec<String>, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    struct V;
-    impl<'de> serde::de::Visitor<'de> for V {
-        type Value = Vec<String>;
-
-        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-            write!(
-                formatter,
-                "a sequence of strings separated by \"{SEPARATOR}\""
-            )
-        }
-
-        fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-        where
-            E: serde::de::Error,
-        {
-            Ok(v.split(SEPARATOR)
-                .filter(|s| !s.is_empty())
-                .map(ToString::to_string)
-                .collect())
-        }
-    }
-
-    deserializer.deserialize_str(V)
 }
 
 async fn new_vote(mut req: Request) -> tide::Result {
---
+++ src/vote.rs
@@ -0,0 +1,68 @@
+use serde::{de::Deserializer, Deserialize};
+
+pub const CHOICE_SEPARATOR: char = '\x1F';
+
+#[derive(Debug, Deserialize)]
+pub struct Vote {
+    pub title: String,
+    #[serde(deserialize_with = "deserialize_choices")]
+    choices: Vec<String>,
+    #[serde(rename(deserialize = "max-choices"))]
+    max_choices: usize,
+    #[serde(default)]
+    anonymous: bool,
+    #[serde(deserialize_with = "deserialize_password")]
+    password: Option<String>,
+}
+
+fn deserialize_password<'de, D>(deserializer: D) -> Result<Option<String>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    struct V;
+    impl<'de> serde::de::Visitor<'de> for V {
+        type Value = Option<String>;
+
+        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+            formatter.write_str("a string")
+        }
+
+        fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+        where
+            E: serde::de::Error,
+        {
+            Ok((!v.is_empty()).then(|| v.to_string()))
+        }
+    }
+
+    deserializer.deserialize_str(V)
+}
+
+fn deserialize_choices<'de, D>(deserializer: D) -> Result<Vec<String>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    struct V;
+    impl<'de> serde::de::Visitor<'de> for V {
+        type Value = Vec<String>;
+
+        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+            write!(
+                formatter,
+                "a sequence of strings separated by \"{CHOICE_SEPARATOR:?}\""
+            )
+        }
+
+        fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+        where
+            E: serde::de::Error,
+        {
+            Ok(v.split(CHOICE_SEPARATOR)
+                .filter(|s| !s.is_empty())
+                .map(ToString::to_string)
+                .collect())
+        }
+    }
+
+    deserializer.deserialize_str(V)
+}
