9ad07554fc7520473451b7adf7f42688851c82d8
isaacthefallenapple <isaacthefallenapple@gmail.com>
2023-09-06 14:47:51 +0200

    serve prelim results page

--- src/results.rs
+++ src/results.rs
@@ -1,5 +1,9 @@
 use crate::Request;
 
 pub async fn get(_req: Request) -> tide::Result {
-    todo!()
+    let mut res = tide::Response::new(tide::StatusCode::Ok);
+    let body: tide::Body = dbg!(tide::Body::from_file("front/results.html").await)?;
+    res.set_body(body);
+
+    Ok(res)
 }
